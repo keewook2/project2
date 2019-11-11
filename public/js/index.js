444deb81f6ff87794cbbf5fffcaa2c703e1e85f7
Abbosjon <squarefozilov@gmail.com>
2019-11-11 13:51:23 -0500

    ajax call

--- public/js/index.js
+++ public/js/index.js
@@ -18,13 +18,11 @@ function ShowTheSectors(){
       var changes =  response.sectorPerformance[item].changesPercentage ; 
       console.log(sector);
       console.log(changes);
-      var SectorPercentage =  "<tr> <th> <a href='/api/company'>+ sector + "</a></th> <th> "+ changes +" </th> </tr>" ; 
+      var SectorPercentage =  "<tr> <th> <a href='/api/company'>"+ sector + "</a></th> <th> "+ changes +" </th> </tr>" ; 
       $(".tbody").append(SectorPercentage);
       });
   });
 }
-
-
 ShowTheSectors();
 
 // The API object contains methods for each kind of request we'll make
